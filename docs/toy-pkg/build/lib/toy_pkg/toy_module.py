76712d8e578512a7702921d6b74458e5bee3c940
Tomas Beuzen <tomas.beuzen@gmail.com>
2020-07-21 10:08:34 -0700

    add toy wheel

---
+++ docs/toy-pkg/build/lib/toy_pkg/toy_module.py
@@ -0,0 +1,3 @@
+def test_function():
+    """Prints a simple statement."""
+    print("You manually installed the toy_pkg example! Well done!")
---
+++ docs/toy-pkg/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="toy_pkg",
+      version="0.0.1",
+      author="Tomas Beuzen",
+      description="An example toy package",
+      packages=["toy_pkg"],
+      )
