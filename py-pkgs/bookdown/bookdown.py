a58228af91fb1c7b45a3980138621a4df16b425e
Tomas Beuzen <tomas.beuzen@gmail.com>
2021-02-23 13:40:09 -0800

    we have a working bookdown render for now

--- py-pkgs/bookdown/bookdown.py
+++ py-pkgs/bookdown/bookdown.py
@@ -1,5 +1,6 @@
 import re
 import os
+from textwrap import dedent
 
 
 class RmdCleaner:
@@ -8,100 +9,83 @@ class RmdCleaner:
         with open(filename) as f:
             self.text = f.read()
 
-    def remove_header(self):
-        header = re.compile("---(?:.*\n)+---", re.DOTALL)
-        self.text = header.sub("", self.text)
-
-    def index_header(self):
-        header = re.compile("---(?:.*\n)+---")
-        new_header = """---
-        title: "Python Packages"
-        author:
-        - Tomas Beuzen
-        - Tiffany Timbers
-        date: "`r Sys.Date()`"
-        documentclass: krantz
-        bibliography: [book.bib, packages.bib]
-        biblio-style: apalike
-        link-citations: yes
-        colorlinks: yes
-        lot: yes
-        lof: yes
-        site: bookdown::bookdown_site
-        description: "Open source book on building Python packages."
-        github-repo: UBC-MDS/py-pkgs
-        graphics: yes
-        ---
-        """
-        self.text = header.sub(new_header, self.text)
-
     def figures(self):
-        figures = re.findall("```{figure}(?:.+\n)+```", self.text, re.MULTILINE)
-        for figure in figures:
-            try:
-                reference = re.findall("name: (.+)\n", figure)[0]
-                caption = re.findall("alt: (.+)\\n---", figure)[0]
-                size = re.findall("width: (.+px)\\nname", figure)[0]
-                file_location = re.findall("images\/.+.(?:png|svg)", figure)[0]
-            except IndexError:
-                print(f"Bad figure formatting:\n\n{figure}")
-            new_figure = f'```{{r {reference}, fig.cap = "{caption}", fig.retina = 2, out.width="{size}", echo = FALSE, message = FALSE, warning = FALSE}}\nknitr::include_graphics("{file_location}")\n```'
-            self.text = re.sub(
-                pattern=figure, repl=new_figure, string=self.text
+        def repl(match):
+            figure = match.group(0)
+            reference = re.findall("name: \d+-(.+)\n", figure)[0]
+            caption = re.findall("alt: (.+)\\n---", figure)[0]
+            size = re.findall("width: (.+)(?=%)", figure)[0]
+            file_location = re.findall("images\/.+.(?:png|svg)", figure)[0]
+            return f'```{{r {self.filename[:2] + "-" + reference}, fig.cap = "{caption}", out.width = "{size}%", fig.retina = 2, fig.align = "center", echo = FALSE, message = FALSE, warning = FALSE}}\nknitr::include_graphics("{"../" + file_location}")\n```'
+        try:
+            self.text = re.sub("```{figure}(?:.+\n)+```", repl, self.text)
+        except Exception as e:
+            print("Bad figure formatting!")
+            print(e)
+
+    def header(self):
+        if self.filename == "index.Rmd":
+            new_header = dedent("""\
+            ---
+            title: "Python Packages"
+            author: "Tomas Beuzen and Tiffany Timbers"
+            date: "`r Sys.Date()`"
+            documentclass: krantz
+            bibliography: [book.bib, packages.bib]
+            biblio-style: apalike
+            link-citations: yes
+            colorlinks: yes
+            lot: yes
+            lof: yes
+            site: bookdown::bookdown_site
+            description: "Open source book on building Python packages."
+            github-repo: UBC-MDS/py-pkgs
+            graphics: yes
+            ---
+            """
             )
-
-    def references(self):
-        """Convert in-text references like {ref}`How to package a Python` to [How to package a Python]"""
-        self.text = re.sub(r"{ref}`(?:\d+|A\d):(.*?)`", r"[\1]", self.text)
-
-    def title_references(self):
-        """Remove MyST referencing syntax like (00:preface)="""
-        titles = re.compile(r"\(\d*:.*\n", re.DOTALL)
-        self.text = titles.sub("", self.text)
-
-    def index_titles(self):
-        """Index titles should have a {-} after them to indicate no numbering"""
-        self.text = re.sub(r"(#.*)\n", r"\1 {-}", self.text)
+            self.text = re.sub("(.+\n)+(?:---)", new_header, self.text, count=1)
+        elif self.filename == "01-introduction.Rmd":
+            self.text = re.sub("(.+\n)+(?:---)", "\\\mainmatter", self.text, count=1)
+        else:
+            self.text = re.sub("(.+\n)+(?:---)", "", self.text, count=1)
 
     def line_spacing(self):
         """Remove double line breaks"""
         self.text = re.sub(r"\n\n\n", r"\n\n", self.text)
+
+    def references(self):
+        """Convert in-text references like {ref}`How-to-package-a-Python` to [How to package a Python]"""
+
+        def repl(match):
+            return "[" + match.group(1).replace("-", " ") + "]"
+
+        self.text = re.sub(r"{ref}`(?:\d+|A\d):(.*?)`", repl, self.text)
+
+    def titles(self):
+        """Remove MyST referencing syntax like (00:preface)="""
+        titles = re.compile(r"\(\d*:.*\n")
+        self.text = titles.sub("", self.text)
+        if self.filename == "index.Rmd" or self.filename == "00-authors.Rmd":
+            self.text = re.sub(r"(#.*)\n", r"\1 {-}", self.text)
 
     def save(self):
         """Save file to disk"""
         with open(self.filename, "w") as f:
             f.write(self.text)
 
-    def clean_index(self):
-        self.index_header()
-        self.title_references()
-        self.index_titles()
-        self.references()
-        self.figures()
-        self.line_spacing()
-        self.save()
-
-    def clean_author(self):
-        self.clean_chapter()
-        self.index_titles()
-        self.save()
-
-    def clean_chapter(self):
-        self.remove_header()
-        self.title_references()
+    def clean(self):
+        self.header()
+        self.titles()
         self.references()
         self.figures()
         self.line_spacing()
         self.save()
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     rmd_files = [f for f in os.listdir() if f.endswith(".Rmd")]
     for f in rmd_files:
         r = RmdCleaner(f)
-        if f == "index.Rmd":
-            r.clean_index()
-        elif f == "00-author.Rmd":
-            r.clean_author()
-        else:
-            r.clean_chapter()
+        r.clean()
+        print(f"Cleaned: {f}")
--- py-pkgs/bookdown/bookdown.sh
+++ py-pkgs/bookdown/bookdown.sh
@@ -1,13 +1,32 @@
-# script to generate bookdown PDF
+# Simple script to generate bookdown PDF.
+# Uses jupytext and some custom Python and regex to parse from .ipynb to .Rmd
 
+### When we're ready for the full book ###
+# # Create .Rmd files from .ipynb files
+# (cd .. ; jupytext --set-formats ipynb,bookdown//Rmd --sync *.ipynb)
+# # Unsync the files (before we make changes to the .Rmd)
+# (cd .. ; jupytext --update-metadata '{"jupytext": null}' *.ipynb)
+# jupytext --update-metadata '{"jupytext": null}' *.Rmd
+# # Rename some files
+# mv 00-preface.Rmd index.Rmd
+# rm welcome.Rmd
+
+### Preface and first 3 chapters for now ###
 # Create .Rmd files from .ipynb files
-(cd .. ; jupytext --set-formats ipynb,bookdown//Rmd --sync *.ipynb)
+(cd .. ; jupytext --set-formats ipynb,bookdown//Rmd --sync 00-preface.ipynb)
+(cd .. ; jupytext --set-formats ipynb,bookdown//Rmd --sync 00-authors.ipynb)
+(cd .. ; jupytext --set-formats ipynb,bookdown//Rmd --sync 01-introduction.ipynb)
+(cd .. ; jupytext --set-formats ipynb,bookdown//Rmd --sync 02-setup.ipynb)
 # Unsync the files (before we make changes to the .Rmd)
-(cd .. ; jupytext --update-metadata '{"jupytext": null}' *.ipynb)
+(cd .. ; jupytext --update-metadata '{"jupytext": null}' 00-preface.ipynb)
+(cd .. ; jupytext --update-metadata '{"jupytext": null}' 00-authors.ipynb)
+(cd .. ; jupytext --update-metadata '{"jupytext": null}' 01-introduction.ipynb)
+(cd .. ; jupytext --update-metadata '{"jupytext": null}' 02-setup.ipynb)
 jupytext --update-metadata '{"jupytext": null}' *.Rmd
 # Rename some files
 mv 00-preface.Rmd index.Rmd
+
 # Parse the .Rmd to change Jupyter Book format to RStudio format
 python bookdown.py
-# # Build the bookdown
-# r bookdown
+# Build the book with bookdown
+rscript bookdown.r
