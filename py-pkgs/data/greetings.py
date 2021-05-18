dfb598a7aa94714b18d65445682307e308874161
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-05-19 08:40:45 +1000

    progress

---
+++ py-pkgs/data/greetings.py
@@ -0,0 +1,5 @@
+name = "Tom"
+
+
+def hello_world():
+    print(f"Hello world, my name is {name}!")
--- py-pkgs/data/hello.py
+++
@@ -1,7 +0,0 @@
-name = "Tom"
-
-print("hi")
-
-
-def hello_world():
-    print(f"Hello world, my name is {name}!")
