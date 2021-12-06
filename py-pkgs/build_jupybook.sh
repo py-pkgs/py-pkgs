ea0af6ca44278ccfadae94ff9452ea0a2ae5f013
Tomas Beuzen <tomas.beuzen@gmail.com>
2021-12-06 21:42:15 +1100

    maint: finished proofs

--- py-pkgs/build_jupybook.sh
+++ py-pkgs/build_jupybook.sh
@@ -8,7 +8,10 @@ jupyter-book build .
 perl -i -pe 's/\\index\{.*?\}//g' ./_build/html/*.html
 
 # replace Latex triple backticks
-perl -i -pe 's/\\`{}\\`{}\\`{}/\\`\\`\\`/g' ./_build/html/*.html
+perl -i -pe 's/`{}`{}`{}/```/g' ./_build/html/*.html
+
+# replace Latex \newpage commands
+perl -i -pe 's/\\newpage//g' ./_build/html/*.html
 
 # # replace bookdown index command \index{*} with *
 # perl -i -pe 's/\\index\{(.*?)\}/\1/g' ./_build/html/*.html
